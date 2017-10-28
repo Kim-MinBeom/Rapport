305cc5a349e96156c88155b26f275190d0abc5e9
Kim-MinBeom <kmb3755@naver.com>
2017-10-28 19:17:09 +0900

    Jump2Java

---
+++ 김민범/Java/Jump2java/workspace/jump2java/src/jump2java/boolenan.java
@@ -0,0 +1,16 @@
+package jump2java;
+
+public class boolenan {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		//Âü°ú °ÅÁþ
+		boolean isodd = a>b;
+		
+		System.out.println(isodd);
+		
+		if(isodd) {
+			System.out.println("´Ä¾ú´Ù");
+		}
+	}
+}
